1bf8ef72884a76a497d3c12406d5d8cbb41e0824
PiotrGoralczyk <pgoralcz@student.agh.edu.pl>
2014-06-06 15:17:24 +0200

    Introduce Spring IoC

--- src/main/java/pl/edu/agh/yapa/controller/AdsController.java
+++ src/main/java/pl/edu/agh/yapa/controller/AdsController.java
@@ -1,14 +1,22 @@
 package pl.edu.agh.yapa.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+import pl.edu.agh.yapa.persistence.AdsDao;
 
 /**
  * @author pawel
  */
 @Controller
 public class AdsController {
+    private final AdsDao adsDao;
+
+    @Autowired
+    public AdsController(AdsDao adsDao) {
+        this.adsDao = adsDao;
+    }
 
     @RequestMapping("/ads")
     public ModelAndView list(){
